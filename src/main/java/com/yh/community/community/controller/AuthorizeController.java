ec5d7d481609fbb26922bb565080a5a7a0349f98
rjshaonian <648481226@qq.com>
2019-11-09 09:30:20 +0800

    xiugai

--- src/main/java/com/yh/community/community/controller/AuthorizeController.java
+++ src/main/java/com/yh/community/community/controller/AuthorizeController.java
@@ -1,39 +1,49 @@
 package com.yh.community.community.controller;
 
-import com.yh.community.community.dto.AccessTokenDTO;
-import com.yh.community.community.dto.GithubUser;
-import com.yh.community.community.provider.GithubProvider;
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.yh.community.community.dto.AccessTokenDTO;
+import com.yh.community.community.dto.GithubUser;
+import com.yh.community.community.provider.GithubProvider;
+
 @Controller
 public class AuthorizeController {
 
-    @Autowired
-    private GithubProvider githubProvider;
+	@Autowired
+	private GithubProvider githubProvider;
 
-    @Value("${github.client.id}")
-    private String clientId;
-    @Value("${github.client.secret}")
-    private String clientSecret;
-    @Value("${github.redirect.uri}")
-    private String redirectUri;
+	@Value("${github.client.id}")
+	private String clientId;
+	@Value("${github.client.secret}")
+	private String clientSecret;
 
-    @GetMapping("/callback")
-    public String callback(@RequestParam(name = "code") String code,
-                           @RequestParam(name = "state") String state) {
-        AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
-        accessTokenDTO.setClient_id(clientId);
-        accessTokenDTO.setClient_secret(clientSecret);
-        accessTokenDTO.setCode(code);
-        accessTokenDTO.setRedirect_uri(redirectUri);
-        accessTokenDTO.setState(state);
-        String accessToken = githubProvider.getAccessToken(accessTokenDTO);
-        GithubUser user = githubProvider.getUser(accessToken);
-        System.out.println(user.getName());
-        return "index";
-    }
+	@Value("${github.redirect.uri}")
+	private String redirectUri;
+
+	@GetMapping("/callback")
+	public String callback(@RequestParam(name = "code") String code, @RequestParam(name = "state") String state,
+			HttpServletRequest request) {
+		AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
+		accessTokenDTO.setClient_id(clientId);
+		accessTokenDTO.setClient_secret(clientSecret);
+		accessTokenDTO.setCode(code);
+		accessTokenDTO.setRedirect_uri(redirectUri);
+		accessTokenDTO.setState(state);
+		String accessToken = githubProvider.getAccessToken(accessTokenDTO);
+		GithubUser user = githubProvider.getUser(accessToken);
+		if (user != null) {
+			// 登录成功，写cookie和session
+			request.getSession().setAttribute("user", user);
+			return "redirect:/";
+		} else {
+			// 登录失败，重新登录
+			return "redirect:/";
+		}
+	}
 }
--- src/main/java/com/yh/community/community/dto/GithubUser.java
+++ src/main/java/com/yh/community/community/dto/GithubUser.java
@@ -1,31 +1,31 @@
 package com.yh.community.community.dto;
 
 public class GithubUser {
-    private String name;
-    private Long id;
-    private String bio;
+	private String name;
+	private Long id;
+	private String bio;
 
-    public String getName() {
-        return name;
-    }
+	public String getName() {
+		return name;
+	}
 
-    public void setName(String name) {
-        this.name = name;
-    }
+	public void setName(String name) {
+		this.name = name;
+	}
 
-    public Long getId() {
-        return id;
-    }
+	public Long getId() {
+		return id;
+	}
 
-    public void setId(Long id) {
-        this.id = id;
-    }
+	public void setId(Long id) {
+		this.id = id;
+	}
 
-    public String getBio() {
-        return bio;
-    }
+	public String getBio() {
+		return bio;
+	}
 
-    public void setBio(String bio) {
-        this.bio = bio;
-    }
+	public void setBio(String bio) {
+		this.bio = bio;
+	}
 }
